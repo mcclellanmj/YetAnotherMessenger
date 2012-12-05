a416aa9858aa489ec71868f2e72fcb3efafb557b
mmcclellan <mmcclellan@franklinamerican.com>
2012-12-05 00:00:33 -0600

    Server routing for logins, layout updated, fixed layout for main messenger

--- WebApps/nodeUi/app.js
+++ WebApps/nodeUi/app.js
@@ -5,8 +5,8 @@ var packageInfo = require('./package.json');
 
 var logFactory = require('simply-log');
 logFactory.setDefaultLevel(logFactory.TRACE).addDefaultAppender(function(name, level, args) {
-		args.unshift(Date.now() + ' ' + name + ':' + level + ' ->');
-		Function.prototype.apply.call(console.log, console, args);
+	args.unshift(Date.now() + ' ' + name + ':' + level + ' ->');
+	Function.prototype.apply.call(console.log, console, args);
 });
 
 var log = logFactory.getLogger('root');
@@ -27,20 +27,47 @@ app.set('view engine', 'jade');
 // Use stylus as css engine
 app.use(stylus.middleware({ src: __dirname + '/public', compile: compile}));
 app.use(express.cookieParser('secr3t'));
+app.use(express.bodyParser());
 
 // Define public directory
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-	var log = logFactory.getLogger('router');
-	if(req.signedCookies.name == undefined) {
-		res.cookie('name', 'john', {signed: true});
-		log.trace('authentication cookie set');
+app.all('/messenger*', function(req, res, next) {
+	var log = logFactory.getLogger('filter');
+	log.trace('entering authentication filter');
+	if(req.signedCookies.token == undefined) {
+		log.trace('No cookie found, redirecting to login');
+		res.redirect('/login');
+		return;
 	}
-  res.render('index', {'packageInfo' : packageInfo});
-  log.debug('User cookie found:', req.signedCookies.name);
+	next();
+});
+
+app.get('/', function (req, res) {
+	var log = logFactory.getLogger('index');
+	log.trace('Redirecting to messenger');
+	res.redirect('/messenger');
+});
+
+app.get('/messenger', function (req, res) {
+	var log = logFactory.getLogger('messenger');
+	res.render('index', {'packageInfo' : packageInfo});
+	log.debug('User cookie found:', req.signedCookies.token);
+});
+
+app.get('/login', function(req, res) {
+	var log = logFactory.getLogger('login');
+	log.trace('Rendering login');
+	res.render('login', {'packageInfo' : packageInfo});
+});
+
+app.post('/login', function(req, res) {
+	var log = logFactory.getLogger('login');
+	log.trace('Login recieved from ' + req.body.username);
+	res.cookie('token', req.body.username, {signed: true});
+	res.redirect('/');
 });
 
 var port = 3000;
 app.listen(port);
-log.info('Application listening on port', port);
+log.info('Application started and listening on port', port);
