9921a7f2ba8a502913878c5d7c3ea70be7c13917
Matt McClellan <mcclellan.mj@gmail.com>
2012-12-07 00:34:52 -0600

    Now passes messages back and forth between the client, in need of cleanup and refactoring now

--- WebApps/nodeUi/app.js
+++ WebApps/nodeUi/app.js
@@ -3,17 +3,13 @@ var app = express();
 var stylus = require('stylus');
 var nib = require('nib');
 var packageInfo = require('./package.json');
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
 var logFactory = require('simply-log');
+var expRedisStore = require('connect-redis')(express);
+var redis = require("redis").createClient();
 logFactory.setDefaultLevel(logFactory.TRACE).addDefaultAppender(function(name, level, args) {
 	args.unshift(Date.now() + ' ' + name + ':' + level + ' ->');
 	Function.prototype.apply.call(console.log, console, args);
 });
-
-var ioInternalLogger = logFactory.getLogger('io.internal');
-io.set('logger', ioInternalLogger);
-io.set('log level', logFactory.TRACE);
 
 var log = logFactory.getLogger('root');
 log.info('Application starting');
@@ -22,31 +18,22 @@ function compile(str, path) {
   return stylus(str).set('filename', path).use(nib());
 }
 
-io.sockets.on('connection', function(socket) {
-	var log = logFactory.getLogger('io');
-	log.trace('got new connection');
-
-	var id = setInterval(function() {
-		log.trace('emitting ', process.memoryUsage());
-		socket.emit('memory', process.memoryUsage());
-	}, 1000);
-
-	socket.on('disconnect', function () {
-		log.trace('disconnect recieved');
-    	clearInterval(id);
-  	});
-});
-
 // Define views directory
 app.set('views', __dirname + '/views');
 
 // Use jade as view engine
 app.set('view engine', 'jade');
 
+var concreteStore = new expRedisStore({ host: 'localhost', port: 6092, client: redis });
 // Use stylus as css engine
 app.use(stylus.middleware({ src: __dirname + '/public', compile: compile}));
-app.use(express.cookieParser('secr3t'));
+var cookieParser = express.cookieParser('s3cr3t');
+app.use(cookieParser);
 app.use(express.bodyParser());
+app.use(express.session({
+      secret: "s3cr3t",
+      store: concreteStore
+}));
 
 // Define public directory
 app.use(express.static(__dirname + '/public'));
@@ -54,7 +41,8 @@ app.use(express.static(__dirname + '/public'));
 app.all('/messenger*', function(req, res, next) {
 	var log = logFactory.getLogger('filter');
 	log.trace('entering authentication filter');
-	if(req.signedCookies.token == undefined) {
+	log.trace('username is ', req.session.username);
+	if(req.session.username == undefined) {
 		log.trace('No cookie found, redirecting to login');
 		res.redirect('/login');
 		return;
@@ -83,10 +71,45 @@ app.get('/login', function(req, res) {
 app.post('/login', function(req, res) {
 	var log = logFactory.getLogger('login');
 	log.trace('Login recieved from ' + req.body.username);
-	res.cookie('token', req.body.username, {signed: true});
+	req.session.username = req.body.username;
 	res.redirect('/');
 });
 
 var port = 3000;
-server.listen(port);
+var server = app.listen(port);
 log.info('Application started and listening on port', port);
+
+var io = require('socket.io').listen(server);
+var RedisStore = require('socket.io/lib/stores/redis')
+  , redis  = require('socket.io/node_modules/redis')
+  , pub    = redis.createClient()
+  , sub    = redis.createClient()
+  , client = redis.createClient();
+
+io.set('store', new RedisStore({
+  redisPub : pub
+, redisSub : sub
+, redisClient : client
+}));
+
+var ioInternalLogger = logFactory.getLogger('io.internal');
+io.set('logger', ioInternalLogger);
+io.set('log level', logFactory.TRACE);
+
+var SessionSockets = require('session.socket.io');
+var sessionSockets = new SessionSockets(io, concreteStore, cookieParser);
+
+sessionSockets.on('connection', function(err, socket, session) {
+	var log = logFactory.getLogger('io');
+	var id = session.username;
+	log.trace('got new connection for ' + id);
+
+	socket.on('clientMessage', function(message) {
+		log.trace('recieved message ', message);
+		io.sockets.emit('serverMessage', id + ": " + message);
+	});
+
+	socket.on('disconnect', function () {
+		log.trace('disconnect recieved');
+  	});
+});
